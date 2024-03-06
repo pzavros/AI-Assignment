50a3bbef57a60e4adf33c8c77be6f57e1f825f7f
Panayiotis Zavros <panazavros8@gmail.com>
2024-03-06 11:01:55 +0200

    load data

---
+++ main.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.stats as stats
+import seaborn as sns
+
+
+
+# Load Data
+cancerData = pd.read_csv("datasets/Cancer_Data.csv")
+breastData = pd.read_csv("datasets/breast-cancer-dataset.csv")
+patientData = pd.read_csv("datasets/cancer patient data sets.csv")
+
