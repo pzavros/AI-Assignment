eea5e00704a23112563047591f9c32a2bd2289a9
Panayiotis Zavros <panazavros8@gmail.com>
2024-03-08 14:36:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import seaborn as sns
 #data visualisation
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
+
 
 
 
@@ -18,27 +20,24 @@ breastData = pd.read_csv("datasets/breast-cancer-dataset.csv")
 patientData = pd.read_csv("datasets/cancer patient data sets.csv")
 
 
-print(cancerData)
-
-
+# histogram
 plt.figure(figsize=(30, 20)) 
 # Adjust the layout to prevent overlapping
 cancerData.hist(bins=15, layout=(8, 4), figsize=(30, 20))
 plt.tight_layout()
 
-
 plt.show()
 
-# histogram
 
+# sellect all
 dep_var = cancerData.iloc[:,:].values 
 
 
-print(ind_vars)
-
 #preproccesing
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean') #replace any missing values by the average of the data in the respective column. 
-imputer.fit(ind_vars[:, 3:12]) #fit method which computes the mean of the columns specified
-ind_vars[:, 3:12] = imputer.transform(ind_vars[:, 3:12]) #the new columns will replace the old columns
-print(ind_vars)
+imputer.fit(dep_var[:,:]) #fit method which computes the mean of the columns specified
+dep_var[:,:] = imputer.transform(dep_var[:,:]) #the new columns will replace the old columns
+print(dep_var)
+
+
 
