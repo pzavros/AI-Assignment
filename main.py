8ff6a3a27c7c8451976704d6b41100030b762024
Dimitar Vasilev <v_dimitar@outlook.com>
2024-03-27 18:19:12 +0200

    MLP, ANN, NAIVE BAYES,

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, recall_score, confusion_matrix
+from sklearn.metrics import accuracy_score, recall_score,  precision_score, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.naive_bayes import GaussianNB
+import tensorflow as tf
 
 # Load Data
 cancerData = pd.read_csv("datasets/Cancer_Data.csv")
@@ -45,6 +47,41 @@ X = cancerData_imputed[numeric_cols].values
 y = cancerData_imputed['diagnosis_encoded'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+#-------MLP ALGORITHM-------#
+
+ann = tf.keras.models.Sequential()
+
+# Add the input layer and the first hidden layer with 6 units and relu activation function
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
+# Add the second hidden layer with 6 units and relu activation function
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
+# Add the output layer with 1 output unit and sigmoid activation function
+ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+# Compile the ANN with adam optimizer, loss function = binary crossentropy, and metrics = accuracy
+ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the ANN on the Training set constructed with batch size 32 and epochs 100
+ann.fit(X_train, y_train, batch_size=32, epochs=100)
+
+
+# Make predictions based on the test set results
+y_pred = ann.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Output the predictions alongside the actual values
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+# Construct the Confusion Matrix and calculate the accuracy
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print("Accuracy Score:", accuracy_score(y_test, y_pred))
+
+
+#--------------------------#
+
 # Balancing the Training Dataset with SMOTE
 smote = SMOTE(random_state=42)
 X_train_balanced, y_train_balanced = smote.fit_resample(X_train, y_train)
@@ -79,6 +116,10 @@ param_grid_svc = {
 grid_search_svc = GridSearchCV(estimator=svc, param_grid=param_grid_svc, cv=5, scoring='accuracy', n_jobs=-1)
 grid_search_svc.fit(X_train_balanced, y_train_balanced)
 
+# Naive Bayes, NO NEED for HYPERPARAMETERS TUNING DEAFAULT GOOD ENOUGH BRO
+
+#
+
 # Print best parameters and scores
 print("\nBest parameters for Logistic Regression:", grid_search_lr.best_params_)
 print("Best score for Logistic Regression:", grid_search_lr.best_score_)
@@ -93,41 +134,51 @@ def print_metrics_and_confusion_matrix(classifier, X_test, y_test, classifier_na
     print(f"\n{classifier_name}")
     print(f"Accuracy: {accuracy_score(y_test, y_pred)*100:.2f}%")
     print(f"Recall: {recall_score(y_test, y_pred)*100:.2f}%")
+    print(f"Precision: {precision_score(y_test, y_pred)*100:.2f}%")
     print("Confusion Matrix:")
     
 # Confusion matrix, example representation
 #     [[True Negatives (TN)  False Positives (FP)]
 #     [False Negatives (FN) True Positives (TP)]]
-    print(confusion_matrix(y_test, y_pred))
+    print(confusion_matrix(y_test, y_pred)) 
 
 # Use best parameters for training
-lr_classifier = LogisticRegression(**grid_search_lr.best_params_, random_state=0)
-rf_classifier = RandomForestClassifier(**grid_search_rf.best_params_, random_state=0)
-svc_classifier = SVC(**grid_search_svc.best_params_, random_state=0)
+logistic_regresion_classifier = LogisticRegression(**grid_search_lr.best_params_, random_state=0)
+random_forest_classifier = RandomForestClassifier(**grid_search_rf.best_params_, random_state=0)
+svc_linear_classifier = SVC(**grid_search_svc.best_params_, random_state=0)
+naive_classifier = GaussianNB()
+
 
 # ------------------------ TRAIN ON UNBALANCED DATA ---------------------------
-lr_classifier.fit(X_train, y_train)
-rf_classifier.fit(X_train, y_train)
-svc_classifier.fit(X_train, y_train)
+logistic_regresion_classifier .fit(X_train, y_train)
+random_forest_classifier .fit(X_train, y_train)
+svc_linear_classifier.fit(X_train, y_train)
+naive_classifier.fit(X_train, y_train)
 
 # Evaluation
 print("\n ----- Unbalanced Data ----- ")
-print_metrics_and_confusion_matrix(lr_classifier, X_test, y_test, "Logistic Regression")
-print_metrics_and_confusion_matrix(rf_classifier, X_test, y_test, "Random Forest")
-print_metrics_and_confusion_matrix(svc_classifier, X_test, y_test, "SVC")
+print_metrics_and_confusion_matrix(logistic_regresion_classifier , X_test, y_test, "Logistic Regression:")
+print_metrics_and_confusion_matrix(random_forest_classifier , X_test, y_test, "Random Forest:")
+print_metrics_and_confusion_matrix(svc_linear_classifier, X_test, y_test, "SVC:")
+print_metrics_and_confusion_matrix(naive_classifier, X_test, y_test, "Naive Baysain:")
 
 # ------------------------ TRAIN ON BALANCED DATA ---------------------------
 lr_classifier_balanced = LogisticRegression(**grid_search_lr.best_params_, random_state=0)
 rf_classifier_balanced = RandomForestClassifier(**grid_search_rf.best_params_, random_state=0)
 svc_classifier_balanced = SVC(**grid_search_svc.best_params_, random_state=0)
+naive_classifier_balanced = GaussianNB()
+
 
 lr_classifier_balanced.fit(X_train_balanced, y_train_balanced)
 rf_classifier_balanced.fit(X_train_balanced, y_train_balanced)
 svc_classifier_balanced.fit(X_train_balanced, y_train_balanced)
+naive_classifier_balanced.fit(X_train_balanced, y_train_balanced)
+
 
 # Evaluation
 print("\n ----- Balanced Data ----- ")
-print_metrics_and_confusion_matrix(lr_classifier_balanced, X_test, y_test, "Logistic Regression")
-print_metrics_and_confusion_matrix(rf_classifier_balanced, X_test, y_test, "Random Forest")
-print_metrics_and_confusion_matrix(svc_classifier_balanced, X_test, y_test, "SVC")
+print_metrics_and_confusion_matrix(lr_classifier_balanced, X_test, y_test, "Logistic Regression:")
+print_metrics_and_confusion_matrix(rf_classifier_balanced, X_test, y_test, "Random Forest:")
+print_metrics_and_confusion_matrix(svc_classifier_balanced, X_test, y_test, "SVC:")
+print_metrics_and_confusion_matrix(naive_classifier_balanced, X_test, y_test, "Naive Baysain:")
 
